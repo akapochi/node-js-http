228657fca642fb568f71665d8952a85d60b9bea8
Soma Akahoshi <redstar.ihsohaka@gmail.com>
2020-09-12 02:17:45 +0900

    「18. 認証で利用者を制限する」まで実装

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const now = new Date();
 
 const http = require('http');
 const pug = require('pug');
+
+const auth = require('http-auth');
+const basic = auth.basic(
+  { realm: 'Enquetes Area.' },
+  (username, password, callback) => {
+    callback(username === 'guest' && password === 'xaXZJQmE');
+  }
+);
 const server = http
-  .createServer((req, res) => {
+  .createServer(basic, (req, res) => {
     console.info(`[${now}] Requested by ${req.connection.remoteAddress}`)
+    
+    if (req.url === '/logout') {
+      res.writeHead(401, {
+        'Content-Type': 'text/plain; charset=utf-8'
+      });
+      res.end('ログアウトしました');
+      return;
+    }
+    
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
     });
