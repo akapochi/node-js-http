7d160b95a9f0d2870b725c45ebf020726a574484
Soma Akahoshi <redstar.ihsohaka@gmail.com>
2020-09-11 23:51:36 +0900

    「16. テンプレートエンジン」まで実装

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const now = new Date();
 
 const http = require('http');
+const pug = require('pug');
 const server = http
   .createServer((req, res) => {
     console.info(`[${now}] Requested by ${req.connection.remoteAddress}`)
@@ -11,18 +12,31 @@ const server = http
 
     switch (req.method) {
       case 'GET':
-        const fs = require('fs');
-        const rs = fs.createReadStream('./form.html');
-        rs.pipe(res);
+        if (req.url === '/enquetes/yaki-shabu') {
+          res.write(pug.renderFile('./form.pug', {
+            path: req.url,
+            firstItem: "焼き肉",
+            secondItem: "しゃぶしゃぶ"
+          }));
+        } else if (req.url === '/enquetes/rice-bread') {
+          res.write(pug.renderFile('./form.pug', {
+            path: req.url,
+            firstItem: "ごはん",
+            secondItem: "パン"
+          }));
+        }
+        res.end();
         break;
       case 'POST':
         let rawData = '';
         req.on('data', (chunk) => {
           rawData += chunk;
         }).on('end', () => {
-          const decoded = decodeURIComponent(rawData);
-          console.info(`${now} 投稿: ${decoded}`);
-          res.write(`<h1>${decoded}が投稿されました。</h1>`)
+          const qs = require('querystring');
+          const answer = qs.parse(rawData);
+          const body = `${answer['name']}さんは${answer['favorite']}に投票しました。`;
+          console.info(`[${now}] ${body}`);
+          res.write(`<h1>${body}</h1>`)
           res.end();
         });
         break;
